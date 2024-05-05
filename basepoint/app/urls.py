53377c613f997e9e3709912a4b8890573eac24dd
gw9968 <gw9968ans@gmail.com>
2024-05-05 23:47:46 +0200

    Utworzenie .forms dla przesyłania danych, dodawanie grafik do bazy

---
+++ basepoint/app/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path
+from .views import * #login_view, register_view,logout_view,create_worker,dashboard,activate,reset_password_view,reset_password,workers_list,delete_worker, my_account,delete_account,edit_account
+from . import views
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
+    path('logout/', logout_view, name='logout'),
+    path('create-worker/', create_worker, name='create_worker'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('activate/<str:token>/', activate, name='activate'),
+    path('reset-password-confirm/<str:token>/', reset_password, name='reset_password_confirm'),
+    path('login/reset_password_view/', reset_password_view, name='reset_password_view'),
+    path('workers-list/', workers_list, name='workers-list'),
+    path('workers-list/delete-worker/<int:id>', delete_worker, name='delete_worker'),
+    path('my-account/', my_account, name='my_account'),
+    path('my-account/delete-account/', delete_account, name='delete_account'),
+    path('my-account/edit-account/', edit_account, name='edit_account'),
+    path('my-account/edit-account-worker/', edit_account, name='edit_account'),
+    path('invoices/', views.invoices, name='invoices'),
+    path('statistics/', views.statistics, name='statistics'),
+    path('', views.welcome, name='welcome'),
+    path('orders-list/', views.orders_list, name='orders_list'),
+###########################################
+    path('dashboard/produkty/', getAllProducts, name="getAllProducts"),
+    path('dashboard/produkty/<id>', getOneProduct, name="getOneProduct"),
+    path('dashboard/edytujProdukt/<id>', editProduct, name="editProduct"),
+    path('kategorie/', getAllCategory),
+    path('kategorie/<id>', getOneCategory),
+    path('dashboard/dodajProdukt', addNewProduct),
+    #path('dashboard/edytujProdukt', editProductProduct),
+    path('usun/<id>', deleteProduct, name="deleteProduct"),
+    path('kat/', getAllCategory),
+
+    # Inne ścieżki URL dla innych widoków
+]
